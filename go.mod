c1d813d9d5353f346e8f1a7ba5012cc671919716
antklim <antklim@gmail.com>
2021-02-25 16:07:37 +1100

    Create invoice added to main

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/antklim/go-dynamodb
 
 go 1.16
 
-require github.com/aws/aws-sdk-go v1.37.17
+require (
+	github.com/aws/aws-sdk-go v1.37.17
+	github.com/google/uuid v1.2.0
+)
--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"context"
 	"log"
+	"time"
 
 	"github.com/antklim/go-dynamodb/dynamo"
 	"github.com/antklim/go-dynamodb/invoice"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/google/uuid"
 )
 
 func main() {
@@ -17,7 +19,27 @@ func main() {
 	repo := dynamo.NewRepository(client)
 	service := invoice.NewService(repo)
 
-	ctx := context.Background()
-	inv, err := service.GetInvoice(ctx, "123")
-	log.Println(inv, err)
+	{
+		// 1. Create invoice
+		now := time.Now()
+		inv := invoice.Invoice{
+			ID:           uuid.New().String(),
+			Number:       "123",
+			CustomerName: "John Doe",
+			Status:       "NEW",
+			Date:         now,
+			Items:        nil,
+			CreatedAt:    now,
+			UpdatedAt:    now,
+		}
+		ctx := context.Background()
+		err := service.StoreInvoice(ctx, inv)
+		log.Println(inv, err)
+	}
+
+	// {
+	// 	ctx := context.Background()
+	// 	inv, err := service.GetInvoice(ctx, "123")
+	// 	log.Println(inv, err)
+	// }
 }
