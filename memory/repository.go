d22dccf26af8ee0f8428dc53496789d92f881643
antklim <antklim@gmail.com>
2021-06-27 12:51:49 +1000

    Simple methods implementation

--- memory/repository.go
+++ memory/repository.go
@@ -13,9 +13,61 @@ type invoices struct {
 	table map[string]invoice.Invoice
 }
 
+func (i *invoices) create(inv invoice.Invoice) error {
+	i.mu.Lock()
+	defer i.mu.Unlock()
+
+	if i.table == nil {
+		i.table = make(map[string]invoice.Invoice)
+	}
+
+	i.table[inv.ID] = inv
+	return nil
+}
+
+func (i *invoices) read(invoiceID string) (*invoice.Invoice, error) {
+	i.mu.Lock()
+	defer i.mu.Unlock()
+
+	if inv, ok := i.table[invoiceID]; ok {
+		return &inv, nil
+	}
+	return nil, nil
+}
+
 type items struct {
 	mu    sync.Mutex
 	table map[string]invoice.Item
+}
+
+func (i *items) create(item invoice.Item) error {
+	i.mu.Lock()
+	defer i.mu.Unlock()
+
+	if i.table == nil {
+		i.table = make(map[string]invoice.Item)
+	}
+
+	i.table[item.ID] = item
+	return nil
+}
+
+func (i *items) read(itemID string) (*invoice.Item, error) {
+	i.mu.Lock()
+	defer i.mu.Unlock()
+
+	if item, ok := i.table[itemID]; ok {
+		return &item, nil
+	}
+	return nil, nil
+}
+
+func (i *items) del(itemID string) error {
+	i.mu.Lock()
+	defer i.mu.Unlock()
+
+	delete(i.table, itemID)
+	return nil
 }
 
 type Repository struct {
@@ -31,50 +83,66 @@ func NewRepository() *Repository {
 }
 
 func (r *Repository) AddInvoice(ctx context.Context, inv invoice.Invoice) error {
-	return errNotImplemented
+	return r.invs.create(inv)
 }
 
 func (r *Repository) GetInvoice(ctx context.Context, invoiceID string) (*invoice.Invoice, error) {
-	return nil, errNotImplemented
+	return r.invs.read(invoiceID)
 }
 
 func (r *Repository) AddItem(ctx context.Context, item invoice.Item) error {
-	return errNotImplemented
+	return r.itms.create(item)
 }
 
 func (r *Repository) GetItem(ctx context.Context, invoiceID, itemID string) (*invoice.Item, error) {
-	return nil, errNotImplemented
+	return r.itms.read(itemID)
 }
 
 func (r *Repository) GetItemProduct(ctx context.Context, invoiceID, itemID string) (*invoice.Product, error) {
-	return nil, errNotImplemented
+	item, err := r.itms.read(itemID)
+	if item == nil || err != nil {
+		return nil, err
+	}
+
+	p := invoice.Product{
+		SKU:   item.SKU,
+		Name:  item.Name,
+		Price: item.Price,
+	}
+
+	return &p, nil
 }
 
 func (r *Repository) DeleteItem(ctx context.Context, invoiceID, itemID string) error {
-	return errNotImplemented
+	return r.itms.del(itemID)
 }
 
-func (r *Repository) GetItemsByStatus(context.Context, invoice.Status) ([]invoice.Item, error) {
+func (r *Repository) GetItemsByStatus(ctx context.Context, status invoice.Status) ([]invoice.Item, error) {
 	return nil, errNotImplemented
 }
 
-func (r *Repository) GetInvoiceItems(context.Context, string) ([]invoice.Item, error) {
+func (r *Repository) GetInvoiceItems(ctx context.Context, invoiceID string) ([]invoice.Item, error) {
 	return nil, errNotImplemented
 }
 
-func (r *Repository) GetInvoiceItemsByStatus(context.Context, string, invoice.Status) ([]invoice.Item, error) {
+func (r *Repository) GetInvoiceItemsByStatus(
+	ctx context.Context, invoiceID string, status invoice.Status) ([]invoice.Item, error) {
+
 	return nil, errNotImplemented
 }
 
-func (r *Repository) UpdateInvoiceItemStatus(ctx context.Context, invoiceID, itemID string, status invoice.Status) error {
+func (r *Repository) UpdateInvoiceItemStatus(
+	ctx context.Context, invoiceID, itemID string, status invoice.Status) error {
+
 	return errNotImplemented
 }
 
 func (r *Repository) UpdateInvoiceItemsStatus(
 	ctx context.Context, invoiceID string, itemIDs []string, status invoice.Status) error {
+
 	return errNotImplemented
 }
 
-func (r *Repository) ReplaceItems(context.Context, string, []invoice.Item) error {
+func (r *Repository) ReplaceItems(ctx context.Context, invoiceID string, newItems []invoice.Item) error {
 	return errNotImplemented
 }
